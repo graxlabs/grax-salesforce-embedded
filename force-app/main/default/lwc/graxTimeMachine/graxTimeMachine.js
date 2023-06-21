e7d41aecdfd4f561ef36657e4e8280c5e4274c12
Joe Gaska <JoeGaska@Gmail.com>
2023-06-21 12:37:04 -0400

    Added GRAX Time Machine LWC
    Moved all things over from embedded to Time Machine to simplify things

--- force-app/main/default/lwc/graxTimeMachine/graxTimeMachine.js
+++ force-app/main/default/lwc/graxTimeMachine/graxTimeMachine.js
@@ -7,18 +7,12 @@ export default class graxEmbedded extends NavigationMixin(LightningElement) {
   @api flexipageRegionWidth;
   @api objectApiName;
   @api recordId;
-  @api sObject;
  
   @api iFrameSize = 400;
 
   @api apiUrl;
   @track iFrameUrl='';
 
-  @api fields;
-  @api setSelection;
-
-  @api childLevels = 1;
-  @api ignoreRecordLevelSecurity = false;
   @api additionalParams = '';
   @api recordsPerPage = 5;
   @api title;
@@ -29,7 +23,7 @@ export default class graxEmbedded extends NavigationMixin(LightningElement) {
     return await loadLWCPaylod({objectName:this.sObject,fields:this.fields,objectId:this.recordId,enforceRowSecurity:!this.ignoreRecordLevelSecurity});
   }
 
-  async connectedCallback() {
+  async connectedCallback() { 
     this.lwcPayload = await this.queryGRAX();
     this.iFrameUrl = `${this.lwcPayload.apiPath}/web/objects/${this.objectApiName}/records/${this.recordId}/latest/versions?embedded=true&${this.getParams()}`;
   }
@@ -37,12 +31,9 @@ export default class graxEmbedded extends NavigationMixin(LightningElement) {
   // Embedded Parameters
   getParams(){
     var paramFields = this.fields;
-    var retVal = `autologin=true&childObject=${this.sObject}&includeFields=true&fields=${encodeURIComponent(paramFields)}`;
+    var retVal = `autologin=true&includeFields=true&fields=${encodeURIComponent(paramFields)}`;
     if (this.title != "" && !this.title!=null && this.title!=undefined) {
       retVal+=`&title=${encodeURIComponent(this.title)}`;
-    }
-    if (this.childLevels!=null && this.childLevels > 0){
-      retVal+=`&level=${this.childLevels}`;
     }
     if (this.recordsPerPage!=null && this.recordsPerPage > 0){
       retVal+=`&perPage=${this.recordsPerPage}`;
@@ -51,14 +42,6 @@ export default class graxEmbedded extends NavigationMixin(LightningElement) {
     if (this.additionalParams != "" && this.additionalParams!=null && this.additionalParams!=undefined) {
       retVal+='&' + this.additionalParams;
     }
-
-    if (this.setSelection == 'Archived Data')
-      retVal += '&deleteSource=grax';
-    else if (this.setSelection == 'Deleted Data')
-      retVal += '&deleteSource=salesforce';
-    else if (this.setSelection == 'Archived + Deleted Data')
-      retVal += '&deleteSource=any';
-
     return retVal;
   }  
 }
